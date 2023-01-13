8916898c60ca9a4bf77aa994ed3c6f1b2b5e1d8e
Marcelo Santos <105805883+marcelooo616@users.noreply.github.com>
2023-01-13 13:06:07 +0000

    abnner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,7 +1,12 @@
+import './Banner.css';
 export default function Banner(){
     return(
         <div className="banner-container">
-            <h1>Banner</h1>
+            <img
+            className="banner" 
+            src="https://cdn.dribbble.com/users/319473/screenshots/4111136/reinvently_insights_3.0.gif"/>
+
+            <h1 className="nome">Marcelo Nascimento|</h1>
         </div>
     )
 }
